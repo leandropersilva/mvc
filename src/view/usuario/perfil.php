c9c754399561d0f6bdf1c95c2d633385163cfc7c
Leandro Silva <leandro12316@outlook.com>
2025-07-04 15:11:50 -0300

    exemplo

--- src/view/usuario/perfil.php
+++ src/view/usuario/perfil.php
@@ -1,2 +1,7 @@
-<?php
-echo 'b';
+<div class="container">
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-6 col-sm-12">
+            <h1>Perfil do usuário</h1>
+        </div>
+    </div>
+</div>
